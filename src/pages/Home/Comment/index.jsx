6346be64f9d046373c105edd56c66b7cdd1b8c6c
meokisama <hoangdinhsang123@gmail.com>
2024-03-06 18:41:57 +0700

    change comments slider direction

--- src/pages/Home/Comment/index.jsx
+++ src/pages/Home/Comment/index.jsx
@@ -1,74 +1,98 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef, useEffect } from "react";
+import { Pagination } from 'swiper/modules';
 
-import "./Comment.scss"
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef, useEffect } from 'react';
+
+import './Comment.scss';
 import { commentsList } from './Comment';
 import SectionTitle from '../../../components/SectionTitle';
+import img from '../../../assets/makein2.png';
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 function Comment() {
-
     const commentRef = useRef(null);
+    const commentimgRef = useRef(null);
+
     useEffect(() => {
         const el = commentRef.current;
-        gsap.fromTo(el, {
-            x: 500,
-            opacity: 0
-        }, {
-            x: 0,
-            opacity: 1,
-            scrollTrigger: {
-                trigger: el,
-                end: 'top 30%',
-                scrub: true,
+        const elimg = commentimgRef.current;
+
+        gsap.fromTo(
+            el,
+            {
+                x: -500,
+                opacity: 0,
             },
-        });
-    }, [])
+            {
+                x: 0,
+                opacity: 1,
+                scrollTrigger: {
+                    trigger: el,
+                    end: 'top 30%',
+                    scrub: true,
+                },
+            },
+        );
+
+        gsap.fromTo(
+            elimg,
+            {
+                x: 500,
+                opacity: 0,
+            },
+            {
+                x: 0,
+                opacity: 1,
+                scrollTrigger: {
+                    trigger: elimg,
+                    end: 'top 30%',
+                    scrub: true,
+                },
+            },
+        );
+    }, []);
 
     return (
         <div className="comment">
             <SectionTitle
-                title={"Bình Luận"}
-                description={"Phần bình luận từ các bên được phỏng vấn. Kéo sang trái/phải để xem nội dung kế tiếp!"}
+                title={'Bình Luận'}
+                description={'Phần bình luận từ các bên được phỏng vấn. Kéo lên xuống để xem nội dung kế tiếp!'}
             />
-            <Swiper
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={'auto'}
-                direction={'vertical'}
-                coverflowEffect={{
-                    rotate: 50,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 1,
-                    slideShadows: true,
-                }}
-                pagination={true}
-                loop={true}
-                modules={[EffectCoverflow, Pagination]}
-                className="mySwiper"
-                ref={commentRef}
-            >
-                {commentsList.map((comment, index) => (
-                    <SwiperSlide key={index}>
-                        <div className='avatar'>
-                            <img alt='' src={comment.avatar} />
-                            <p>@{comment.username}</p>
-                        </div>
-                        <p className='content'>{comment.content}</p>
-                        <span>━ <span className='author'>{comment.author}</span> ━</span>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            <div className="commentContent">
+                <Swiper
+                    direction={'vertical'}
+                    loop={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                    ref={commentRef}
+                >
+                    {commentsList.map((comment, index) => (
+                        <SwiperSlide key={index}>
+                            <div className="slideContent">
+                                <div className="avatar">
+                                    <img alt="" src={comment.avatar} />
+                                    <p>@{comment.username}</p>
+                                </div>
+                                <p className="content">{comment.content}</p>
+                                <span>
+                                    ━ <span className="author">{comment.author}</span> ━
+                                </span>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className="commentImg" ref={commentimgRef}>
+                    <img alt="" src={img} />
+                </div>
+            </div>
         </div>
     );
 }
