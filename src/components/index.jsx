d95ce9c7a3a9e877e4449ee4e087a9e7b0926973
meokisama <hoangdinhsang123@gmail.com>
2024-03-20 15:01:55 +0700

    minimize import components

---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+export { default as Footer } from './Footer';
+export { default as MessageHub } from './MessageHub';
+export { default as SplashScreen } from './SplashScreen';
+export { default as Transition } from './Transition';
+export { default as DocumentTitle } from './DocumentTitle';
---
+++ src/pages/Vote/List/index.jsx
@@ -0,0 +1,6 @@
+export { default as anticipatedRanobe } from './AnticipatedRanobe';
+export { default as copyrightRanobe } from './CopyrightRanobe';
+export { default as disappointingRanobe } from './DisappointingRanobe';
+export { default as favoriteRanobe } from './FavoriteRanobe';
+export { default as rookieRanobe } from './RookieRanobe';
+export { default as favoritePublisher } from './FavoritePublisher';
