b87b254c0823b9bb7c60e8ca714e7a2437a0ae77
meokisama <hoangdinhsang123@gmail.com>
2024-04-22 10:08:24 +0700

    use fontfaceobserver to load fonts

--- src/index.jsx
+++ src/index.jsx
@@ -9,10 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const font1 = new FontFaceObserver('Playfair Display');
 const font2 = new FontFaceObserver('Lexend');
-const font3 = new FontFaceObserver('Estonia');
-const font4 = new FontFaceObserver('Phudu');
+const font3 = new FontFaceObserver('Phudu');
 
-Promise.all([font1.load(), font2.load(), font3.load(), font4.load()]).then(() => {
+Promise.all([font1.load(), font2.load(), font3.load()]).then(() => {
     root.render(
         <React.StrictMode>
             <GlobalStyles>
