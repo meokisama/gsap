90cc0a9daa830459c6ac027be127f37d1b1c0aa9
meokisama <hoangdinhsang123@gmail.com>
2024-04-25 00:58:08 +0700

    add table header to leaderboard

--- src/components/leaderboard/LeaderBoardTable/index.jsx
+++ src/components/leaderboard/LeaderBoardTable/index.jsx
@@ -1,12 +1,11 @@
 import React, { useRef, useState } from 'react';
-import { SearchOutlined } from '@ant-design/icons';
-import { Select, Table, Button, Input, Space } from 'antd';
+import { SearchOutlined, UserOutlined } from '@ant-design/icons';
+import { Table, Button, Input, Space, Popover, Statistic, Rate } from 'antd';
 import Highlighter from 'react-highlight-words';
 import './LeaderBoardTable.scss';
 import gold from 'assets/gold.png';
 import silver from 'assets/silver.png';
 import bronze from 'assets/bronze.png';
-import sakura from 'assets/sakura1.svg';
 import {
     publisherFilter,
     rookieRanobe,
@@ -99,7 +98,7 @@ function LeaderBoardTable() {
         filterIcon: (filtered) => (
             <SearchOutlined
                 style={{
-                    color: filtered ? '#1677ff' : undefined,
+                    color: filtered ? '#494949d7' : undefined,
                 }}
             />
         ),
@@ -209,6 +208,23 @@ function LeaderBoardTable() {
         },
     ];
 
+    const popoverContent = (
+        <div className="popover-content">
+            {selectArray.map((item) => (
+                <p
+                    key={item.value}
+                    className={selectedCategory.value === item.value ? 'selected-category' : ''}
+                    onClick={() => handleChange(item.value)}
+                >
+                    {item.label}
+                </p>
+            ))}
+        </div>
+    );
+
+    const [rateValue, setRateValue] = useState(4.5);
+    const rateDesc = ['Tốt', 'Quá tốt', 'Cực kỳ tốt', 'Tốt không còn gì để nói', 'G Cup'];
+
     const handleChange = (value) => {
         setSelectedCategory(selectArray.find((item) => item.value === value));
         setData(dataInput[value]);
@@ -217,30 +233,52 @@ function LeaderBoardTable() {
     return (
         <div className="leaderboard">
             <p className="ld-description">
-                Đã có kết quả cho đợt bình chọn bảng xếp hạng kì này! Hãy chọn hạng mục tương ứng ở bên dưới để xem thứ
+                Đã có kết quả cho bảng xếp hạng kì này! Hãy ấn vào nút cài đặt bên cạnh tên hạng mục để chọn và xem thứ
                 hạng cụ thể của hạng mục đó. Có thể ấn vào phần tiêu đề của mỗi cột bên dưới để{' '}
                 <strong>xem thứ hạng tăng dần/giảm dần</strong>, <strong>tìm kiếm tác phẩm cụ thể</strong> hay{' '}
                 <strong>lọc ra các tác phẩm của mỗi nhà xuất bản</strong> tương ứng.
             </p>
-            <div className="ld-select">
-                <Select
-                    defaultValue="favoriteRanobe"
-                    style={{
-                        width: 400,
-                    }}
-                    onChange={handleChange}
-                    options={selectArray}
-                    dropdownStyle={{ fontFamily: 'Lexend' }}
-                    status="error"
-                />
+            <div className="ld-table-header">
+                <div className="header-left-panel">
+                    <div className="category-title">
+                        <h1>{selectedCategory.label}</h1>
+                        <p className="h1-description">
+                            Hạng mục này có tổng cộng <strong>{data.length} tác phẩm</strong> tham gia!
+                        </p>
+                    </div>
+                    <Popover placement="bottomRight" content={popoverContent} trigger="click">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </Popover>
+                </div>
+                <div className="header-right-panel">
+                    <div className="vote-container">
+                        <p>Số phiếu bình chọn</p>
+                        <div className="vote-number">
+                            <UserOutlined />
+                            <p>4128</p>
+                        </div>
+                    </div>
+                    <div className="rate-container">
+                        <p>Đánh giá bảng xếp hạng</p>
+                        <Rate allowHalf tooltips={rateDesc} value={rateValue} />
+                    </div>
+                    <div className="next-container">
+                        <p>Kỳ tiếp theo</p>
+                        <span>Tháng 12/2024</span>
+                    </div>
+                </div>
             </div>
-            <div className="sakuraimg">
-                <img src={sakura} alt="" />
-            </div>
-            <h1>{selectedCategory.label}</h1>
-            <p className="h1-description">
-                [Hạng mục này có tổng cộng <strong>{data.length} tác phẩm</strong> tham gia!]
-            </p>
             <div className="ld-table">
                 <Table
                     columns={columns}
